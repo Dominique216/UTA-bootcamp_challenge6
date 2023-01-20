124c4c5434d067c94a379d7ee7a5179fffbbab0a
Dominique Nix <dominiquenix@Dominiques-MacBook-Air.local>
2023-01-20 13:26:13 -0600

    added functionality

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,46 @@
-// add last-search class to the last search for css properties 
 
-// weather API: will need to use list.weather.temp&humidity list.wind.speed, for each day that is gives 
-
-// set the variable that you get and add the correct classes so it shows up the way you want it to
-
-
-
-// add event listener on the search btn...need to store the input in the local storage so that is saves...need to create a get function that will get the last saved input and append a button with the textcont from thelocal storage and add a class of last search to the last-search-sectiion(so i=ll need a var that targets the last-searchsection so I can append the new button section to it)... need to render a function that will search the api call...need to use the geo api as well so I can get the location of the city that is enterd...then need to insert the coordiniates in the weather api to get the right location... looks at module 6 for instructions...once the api is called need to interate through the array/obj to find the info i want, then create avariable to hold that info. get the value of the info and set to text content. will need to append the forecast to the page for each day and add the necceary clases to each element that is created so that the info will be formatted correctly 
 
 var search = document.querySelector('.search-btn');
 var city = document.getElementById('city');
-console.log(city)
+
+function setLastSearch() {
+    lastSeach = {
+        lastCity: city.value.toUpperCase()
+    }
+
+    localStorage.setItem('lastSearch', JSON.stringify(lastSeach))
+}
+
+function getLastSearch() {
+    var lastCitySearch = JSON.parse(localStorage.getItem("lastSearch"))
+    $('.last-search-section').append(`<button class="last-search">${lastCitySearch.lastCity}</button>`)
+
+}
+
+
+function getCurrWeather(lat, lon) {
+    currWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=43d8ab20052afee7eb5ccc2b3db69764&units=imperial'
+
+    fetch(currWeatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            var currtempinfo = data.main.temp
+            var currwindinfo = data.wind.speed
+            var currhuminfo = data.main.humidity
+
+        var currTemp = $('.curr-temp1')
+        var currWind = $('.curr-wind1')
+        var currHum = $('.curr-hum1')
+
+        currTemp[0].textContent = 'Temp: '+ currtempinfo
+        currWind[0].textContent = 'Wind: '+ currwindinfo
+        currHum[0].textContent = 'Humidity: '+ currhuminfo
+        })
+}
+
 
 function getWeather(lat, lon) {
     weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=43d8ab20052afee7eb5ccc2b3db69764&units=imperial'
@@ -20,44 +50,96 @@ function getWeather(lat, lon) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            var temp1 = data.list[2].main.temp
-            var wind1 = data.list[2].wind.speed
-            var hum1 = data.list[2].main.humidity
-            var temp2 = data.list[10].main.temp
-            var wind2 = data.list[10].wind.speed
-            var hum2 =data.list[10].main.humidity
-            var temp3 = data.list[18].main.temp
-            var wind3 = data.list[18].wind.speed
-            var hum3 =data.list[18].main.humidity
-            var temp4 = data.list[26].main.temp
-            var wind4 = data.list[26].wind.speed
-            var hum4 =data.list[26].main.humidity
-            var temp5 = data.list[34].main.temp
-            var wind5 = data.list[34].wind.speed
-            var hum5 =data.list[34].main.humidity
-
+            var temp1info = data.list[2].main.temp
+            var wind1info = data.list[2].wind.speed
+            var hum1info = data.list[2].main.humidity
+            var temp2info = data.list[10].main.temp
+            var wind2info = data.list[10].wind.speed
+            var hum2info =data.list[10].main.humidity
+            var temp3info = data.list[18].main.temp
+            var wind3info = data.list[18].wind.speed
+            var hum3info =data.list[18].main.humidity
+            var temp4info = data.list[26].main.temp
+            var wind4info = data.list[26].wind.speed
+            var hum4info =data.list[26].main.humidity
+            var temp5info = data.list[34].main.temp
+            var wind5info = data.list[34].wind.speed
+            var hum5info =data.list[34].main.humidity
+        var temp2 = $('.temp2')
+        var wind2 = $('.wind2')
+        var hum2 = $('.hum2')
+        var temp3 = $('.temp3')
+        var wind3 = $('.wind3')
+        var hum3 = $('.hum3')
+        var temp4 = $('.temp4')
+        var wind4 = $('.wind4')
+        var hum4 = $('.hum4')
+        var temp5 = $('.temp5')
+        var wind5 = $('.wind5')
+        var hum5 = $('.hum5')
+        var temp6 = $('.temp6')
+        var wind6 = $('.wind6')
+        var hum6 = $('.hum6')
+        temp2[0].textContent = 'Temp: '+ temp1info
+        wind2[0].textContent = 'Wind: '+ wind1info
+        hum2[0].textContent = 'Humidity: '+ hum1info
+        temp3[0].textContent = 'Temp: '+ temp2info
+        wind3[0].textContent = 'Wind: '+ wind2info
+        hum3[0].textContent = 'Humidity: '+ hum2info
+        temp4[0].textContent = 'Temp: '+ temp3info
+        wind4[0].textContent = 'Wind: '+ wind3info
+        hum4[0].textContent = 'Humidity: '+ hum3info
+        temp5[0].textContent = 'Temp: '+ temp4info
+        wind5[0].textContent = 'Wind: '+ wind4info
+        hum5[0].textContent = 'Humidity: '+ hum4info
+        temp6[0].textContent = 'Temp: '+ temp5info
+        wind6[0].textContent = 'Wind: '+ wind5info
+        hum6[0].textContent = 'Humidity: '+ hum5info
         })
-        
+       
 }
 
 function getCityCoordinates () {
-    requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city.value +'&limit=1&appid=43d8ab20052afee7eb5ccc2b3db69764';
+    requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city.value.toUpperCase() +'&limit=1&appid=43d8ab20052afee7eb5ccc2b3db69764';
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         }) 
         .then(function (data) {
-            console.log(data)
             var lat = data[0].lat;
             var lon = data[0].lon;
             getWeather(lat, lon);
+            getCurrWeather(lat, lon)
         })
 }
 
+function getDay() {
+    var date = dayjs().format('MM/DD/YYYY')
+    var currDay = $('.current-title')
+    currDay[0].textContent = city.value.toUpperCase() + ' ' + date
 
+    var nextFiveDays = $('h4')
+    for(let i = 0; i < nextFiveDays.length; i++) {
+        nextFiveDays[i].textContent = dayjs().add([i], 'day')
+    }
+}
+
+function removeNone() {
+    $('#currWeather').removeClass('none')
+    $('#allCards').removeClass('none')
+}
 
 search.addEventListener('click', (e) => {
     e.preventDefault()
     getCityCoordinates()
-})
+    getDay()
+    setLastSearch()
+    getLastSearch()
+    removeNone()
+}) 
+
+function init() {
+    getLastSearch()
+}
+
+init()
